51e827fee6f0582641f4ded325e0a590ec7b0356
ksharmagit <61244571+ksharmagit@users.noreply.github.com>
2020-02-21 13:24:11 -0500

    Separated the key field

--- SimpleWeather.py
+++ SimpleWeather.py
@@ -4,14 +4,15 @@ import requests
 import json
 from pprint import pprint
 
-# Get your own key from OWM. There's a free plan as well
+# Get your own key from OWM
 # https://home.openweathermap.org/users/sign_up
-key = ''
+key = '91aaa004130cac85afb4c8e38938c48d'
+zip = '20500'
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=23059,us&APPID=' + key)
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&APPID=' + key)
 
 data = json.loads(r.text)
-##pprint(data)
+pprint(data)
 
 print ("The temperature is", "{0:.3f}".format(((float)(data['main']['temp'])-273)*9/5+32), "F")
 print ("The temperature is", "{0:.2f}".format(((float)(data['main']['temp'])-273)), "C")
